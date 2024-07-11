c0671afb2724e0e9b7d9542cbbea92b4a6a7f0c6
Erik Bonifaz <erikbo9@hotmail.com>
2024-07-11 09:06:24 -0600

    feat: new section added

--- scripts/form.js
+++ scripts/form.js
@@ -3,5 +3,6 @@ let $form = document.querySelector("#contactForm");
 document.addEventListener("submit", (e) => {
   if (e.target === $form) {
     e.preventDefault();
+    openTab(e, "thanks");
   }
 });
--- scripts/logic.js
+++ scripts/logic.js
@@ -9,3 +9,51 @@ document.getElementById("back-to-top-arrow2").addEventListener("click", () => {
 function backToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+let $btnInteresa = document.querySelector(".interesa");
+let $btnInteresa2 = document.querySelector(".interesa2");
+let $btnInteresa3 = document.querySelector(".interesa3");
+let $btnInteresa4 = document.querySelector(".interesa4");
+
+$btnInteresa.addEventListener("click", () => {
+  console.log("btnMeInteresa");
+  let value = $btnInteresa.getAttribute("data-paquete");
+  console.log(value);
+  selectOption(value);
+});
+
+$btnInteresa2.addEventListener("click", () => {
+  console.log("btnMeInteresa2");
+  let value = $btnInteresa2.getAttribute("data-paquete");
+  console.log(value);
+  selectOption(value);
+});
+
+$btnInteresa3.addEventListener("click", () => {
+  console.log("btnMeInteresa2");
+  let value = $btnInteresa3.getAttribute("data-paquete");
+  console.log(value);
+  selectOption(value);
+});
+
+$btnInteresa4.addEventListener("click", () => {
+  console.log("btnMeInteresa2");
+  let value = $btnInteresa4.getAttribute("data-paquete");
+  console.log(value);
+  selectOption(value);
+});
+
+// Cambiar valor del formulario al clickear el botón
+function selectOption(value) {
+  console.log(value, "valor recibido");
+  const selectElement = document.getElementById("interest");
+  selectElement.value = value;
+}
+
+//limitar option size
+
+var e = document.querySelectorAll("option");
+e.forEach((x) => {
+  if (x.textContent.length > 20)
+    x.textContent = x.textContent.substring(0, 20) + "...";
+});
