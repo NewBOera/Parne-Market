e9476cee3af91df2ffa9deddb87a4fa1ea5563a9
Erik Bonifaz <erikbo9@hotmail.com>
2024-07-12 08:27:40 -0600

    fix: responsive cards

--- scripts/logic.js
+++ scripts/logic.js
@@ -9,13 +9,19 @@ document.getElementById("back-to-top-arrow2").addEventListener("click", () => {
 function backToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
-
+let $btnContacto = document.querySelector(".btn-contacto");
 let $btnInteresa = document.querySelector(".interesa");
 let $btnInteresa2 = document.querySelector(".interesa2");
 let $btnInteresa3 = document.querySelector(".interesa3");
 let $btnInteresa4 = document.querySelector(".interesa4");
 let $btnInteresa5 = document.querySelector(".interesa5");
 let $btnInteresa6 = document.querySelector(".interesa6");
+
+$btnContacto.addEventListener("click", () => {
+  let value = $btnInteresa.getAttribute("data-paquete");
+
+  selectOption(value);
+});
 
 $btnInteresa.addEventListener("click", () => {
   let value = $btnInteresa.getAttribute("data-paquete");
@@ -53,6 +59,11 @@ $btnInteresa6.addEventListener("click", () => {
   selectOption(value);
 });
 
+$btnContacto.addEventListener("click", () => {
+  let value = $btnContacto.getAttribute("data-paquete");
+  console.log(value);
+  selectOption(value);
+});
 // Cambiar valor del formulario al clickear el botón
 function selectOption(value) {
   const selectElement = document.getElementById("interest");
